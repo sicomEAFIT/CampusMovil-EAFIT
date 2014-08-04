76fa6ad2b2a074847005736bbb697257fde5494e
Mateo Olaya Bernal <molayab@Mateos-MacBook-Air.local>
2014-08-04 00:01:53 -0500

    Base de la aplicacion

---
+++ iOS/CampusMovil/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CampusMovil
+//
+//  Created by Mateo Olaya Bernal on 8/3/14.
+//  Copyright (c) 2014 Mateo Olaya Bernal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/RegisterTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  RegisterTableViewController.h
+//  CampusMovil
+//
+//  Created by Mateo Olaya Bernal on 8/3/14.
+//  Copyright (c) 2014 Mateo Olaya Bernal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterTableViewController : UITableViewController
+{
+    UITextField * username;
+    UITextField * password;
+    UITextField * password_confirmation;
+    UITextField * email;
+}
+
+@end
